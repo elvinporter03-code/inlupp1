095eae4f30a94b3882b3a26be1e399ab9837642c
elvinporter03-code <elvinporter03@gmail.com>
2026-09-14 08:56:01 +0200

    skelettkoden

--- hash_table.c
+++ hash_table.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 typedef struct entry entry_t;
 typedef struct hash_table ioopm_hash_table_t;
 
@@ -12,3 +14,21 @@ struct hash_table
 {
   entry_t *buckets[17];
 };
+
+ioopm_hash_table_t *ioopm_hash_table_create(void) {
+  // TODO: Stub
+  return NULL
+  
+  ;
+ }
+ 
+ void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+  // TODO: Stub
+  (void) ht;
+  return;
+ }
+
+ //$ gcc -Wall -Wextra -g -c hash_table.c kompileringsförslag
+
+
+
